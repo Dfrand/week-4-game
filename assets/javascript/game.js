1062a2c7e9dd0077074522ff56155d642cef04dd
derek <d.frand@yahoo.com>
2017-12-11 14:29:57 -0600

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,139 @@
-// Declare global variables.
-var goalNumber = "";
-var gemNumber = "";
-var addedScore = "";
-var scoring;
-
-$(document).ready(function() {
-
-    // Function for goalNumber.
-    var minNumber = 40;
-    var maxNumber = 120;
-
-    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-
-    function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-
-    $(".goal").append("<div></div>" + randomNumber);
-
-    console.log(goalNumber);
-    console.log(randomNumber);
+// Global variable
+//--------------------------------------------
 
 
-    // Function for gem1.    
-    var minNumber = 1;
-    var maxNumber = 12;
+// Crystal Variables
 
-    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+var crystal = {
+    blue: {
+        name: "Blue",
+        value: 0
+    },
+    red: {
+        name: "Red",
+        value: 0
+    },
+    green: {
+        name: "Green",
+        value: 0
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0
+    }
+};
 
-    function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
+// Scores (Current and Target)
+var currentScore = 0;
+var targetScore = 0;
 
-    $(".gem1").append("<span></span>" + randomNumber);
-    console.log(randomNumber);
-
-    $(".number").on("click", function() {
-        $("#addedScore").text($(this).text());
-        console.log(addedScore);
-    });
-
-
-    // Function for gem2.
-    var minNumber = 1;
-    var maxNumber = 12;
-
-    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-
-    function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-
-    $(".gem2").append("<span></span>" + randomNumber);
-    console.log(randomNumber);
+// Wins and Losses
+var win
+winCount = 0;
+lossCount = 0;
 
 
-    // Function for gem3.
-    var minNumber = 1;
-    var maxNumber = 12;
-
-    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-
-    function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
-
-    $(".gem3").append("<span></span>" + randomNumber);
-    console.log(randomNumber);
+// Functions
+//--------------------------------------------
 
 
-    // Function for gem4.
-    var minNumber = 1;
-    var maxNumber = 12;
+// get random numbers
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+// starts and restarts
+var startGame = function() {
+    // reset the CurrentScore
+    currentScore = 0;
 
-    function randomNumberFromRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    };
+    //set a new TargetScore
+    targetScore = getRandom(19, 120);
 
-    $(".gem4").append("<span></span>" + randomNumber);
-    console.log(randomNumber);
+    // set values to crystals
+    crystal.blue.value = getRandom(1, 12);
+    crystal.red.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+
+    // change html to show changes
+    $('#yourScore').html('<h3>' + currentScore);
+    $('#targetScore').html('<h3>' + targetScore);
 
 
-    // Function for addedScore.
-    // Function for scoring.
+    console.log("------------------------")
+    console.log("Target Score: " + targetScore);
+    console.log("Blue: " + crystal.blue.value + " | Red: " + crystal.red.value + " | Green: " + crystal.green.value + " | Yellow: " + crystal.yellow.value);
+    console.log("---------------------")
+
+}
 
 
+// crystal clicks
+var addValues = function(crystal) {
+
+    // change currentscore
+    currentScore = currentScore + crystal.value;
+
+    // change html
+    $('#yourScore').html('<h3>' + currentScore);
+
+    // call checkWin
+    checkWin();
+
+    console.log('yourScore: ' + currentScore);
+}
+
+// check is user has won
+var checkWin = function() {
+
+    // Check if currentScore is larger than targetScore
+    if (currentScore > targetScore) {
+        alert('Sorry. You lost!');
+        console.log('You lost');
+
+        // add to loss counter
+        lossCount++;
+
+        // change loss html
+        $("#lossCount").html('<h3>' + lossCount);
+
+        // restart game
+        startGame();
+    } else if (currentScore == targetScore) {
+        alert('Congratulations! You Won')
+        console.log('You Win')
+
+        // add to win counter
+        winCount++;
+
+        // change win html
+        $("#winCount").html('<h3>' + winCount)
+
+        // restart game
+        startGame();
+    }
+}
 
 
+// Main Process
+//--------------------------------------------
 
+// Starts the game the first time
+startGame();
+
+$('#blue').click(function() {
+    addValues(crystal.blue);
+});
+
+$('#red').click(function() {
+    addValues(crystal.red);
+});
+
+$('#green').click(function() {
+    addValues(crystal.green);
+});
+
+$('#yellow').click(function() {
+    addValues(crystal.yellow);
 });
